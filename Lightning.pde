2f2eab71f844692ace0a2df6a4a7b9f181f0990f
unknown <releung1@697-334-Umbar.sf.k12.internal>
2015-09-02 11:36:51 -0700

    Added person dodging and changed CSS

--- Lightning.pde
+++ Lightning.pde
@@ -3,6 +3,9 @@ int startY = 0;
 int endX = 0;
 int endY = 150;
 
+int personX = 0;
+int randPos = (int)(Math.random()*100)-50;
+
 int randB = (int)(Math.random()*100);
 
 void setup()
@@ -11,8 +14,10 @@ void setup()
   strokeWeight(1.5);
   background(40, 40, 40);
 }
+
 void draw()
 {
+	//lightning bolt
 	stroke(255,255,randB);
 	while (endY < 300)
 	{
@@ -21,9 +26,23 @@ void draw()
 		line(startX, startY, endX, endY);
 		startX = endX;
 		startY = endY;
-
 	}
+	//little person
+	fill(255);
+	if (startX > 250){
+		personX = endX - 70;
+	} else if (startX < 110){
+		personX = endX + 70;
+	} else {
+		personX = startX + randPos;
+	}
+	noStroke();
+	ellipse(personX, 290, 10, 20);
+	fill(0);
+	ellipse(personX-2, 285, 2, 2);
+	ellipse(personX+2, 285, 2, 2);
 }
+
 void mousePressed()
 {
 	background(40, 40, 40);
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -19,6 +19,9 @@ int startY = 0;
 int endX = 0;
 int endY = 150;
 
+int personX = 0;
+int randPos = (int)(Math.random()*100)-50;
+
 int randB = (int)(Math.random()*100);
 
 public void setup()
@@ -27,8 +30,10 @@ public void setup()
   strokeWeight(1.5f);
   background(40, 40, 40);
 }
+
 public void draw()
 {
+	//lightning bolt
 	stroke(255,255,randB);
 	while (endY < 300)
 	{
@@ -37,9 +42,23 @@ public void draw()
 		line(startX, startY, endX, endY);
 		startX = endX;
 		startY = endY;
-
 	}
+	//little person
+	fill(255);
+	if (startX > 250){
+		personX = endX - 70;
+	} else if (startX < 110){
+		personX = endX + 70;
+	} else {
+		personX = startX + randPos;
+	}
+	noStroke();
+	ellipse(personX, 290, 10, 20);
+	fill(0);
+	ellipse(personX-2, 285, 2, 2);
+	ellipse(personX+2, 285, 2, 2);
 }
+
 public void mousePressed()
 {
 	background(40, 40, 40);
